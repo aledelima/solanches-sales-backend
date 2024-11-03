9b4127a221bd66e4ac4a398ca5fbef29591e5aae
Alessandro Lima <alessandroslima@hotmail.com>
2024-11-03 13:28:23 -0800

    kick off

---
+++ src/main/java/br/com/solanches/solanches_sales_backend/SolanchesSalesBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.solanches.solanches_sales_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolanchesSalesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolanchesSalesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solanches/solanches_sales_backend/SolanchesSalesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solanches.solanches_sales_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolanchesSalesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
